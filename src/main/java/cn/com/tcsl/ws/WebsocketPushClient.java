25fa674a8faeb04a8c7d99b0da8c6ad12ce02ba7
tonyzhu <zhutie8111@163.com>
2018-11-03 11:32:34 +0800

    增加基础文件

---
+++ src/main/java/cn/com/tcsl/ws/WebsocketPushClient.java
@@ -0,0 +1,7 @@
+package cn.com.tcsl.ws;
+
+/**
+ * Created by Administrator on 2018/11/3.
+ */
+public class WebsocketPushClient {
+}
---
+++ src/test/java/cn/com/tcsl/ws/Test.java
@@ -0,0 +1,8 @@
+package cn.com.tcsl.ws;
+
+/**
+ * Created by Administrator on 2018/11/3.
+ */
+
+public class Test {
+}
